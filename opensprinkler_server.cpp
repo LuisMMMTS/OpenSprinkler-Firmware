9a779f25f3f3e4f99b438457c80bf5aa267e5cc2
LuisMMMTS <torres.luismiguel@gmail.com>
2025-10-19 18:51:51 +0100

    Add fertigation data to /jp program output
    - Include fertigation duration data as 7th element in program array
    - Output fertigation value in seconds for each station
    - Output 0 for stations without fertigation enabled
    - This allows frontend to read fertigation data from program array
    
    Program array format now: [type, days0, days1, [starttimes], [durations], name, [fertigation], [daterange]]

--- opensprinkler_server.cpp
+++ opensprinkler_server.cpp
@@ -1179,7 +1179,21 @@ void server_json_programs_main(OTF_PARAMS_DEF) {
 		// program name
 		strncpy(tmp_buffer, prog.name, PROGRAM_NAME_SIZE);
 		tmp_buffer[PROGRAM_NAME_SIZE] = 0;	// make sure the string ends
-		bfill.emit_p(PSTR("$S\",[$D,$D,$D]]"), tmp_buffer,prog.en_daterange,prog.daterange[0],prog.daterange[1]);
+		bfill.emit_p(PSTR("$S\",["), tmp_buffer);
+		// fertigation data
+		for (i=0; i<os.nstations-1; i++) {
+			if (prog.fert[i].enabled) {
+				// Output fertigation duration in seconds
+				bfill.emit_p(PSTR("$D,"), (int)prog.fert[i].value);
+			} else {
+				bfill.emit_p(PSTR("0,"));
+			}
+		}
+		if (prog.fert[i].enabled) {
+			bfill.emit_p(PSTR("$D],[$D,$D,$D]]"), (int)prog.fert[i].value, prog.en_daterange,prog.daterange[0],prog.daterange[1]);
+		} else {
+			bfill.emit_p(PSTR("0],[$D,$D,$D]]"), prog.en_daterange,prog.daterange[0],prog.daterange[1]);
+		}
 		if(pid!=pd.nprograms-1) {
 			bfill.emit_p(PSTR(","));
 		}
